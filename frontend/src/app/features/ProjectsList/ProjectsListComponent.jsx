9b7f5ae11d069b22a0edb0af7bd270a3fe40f56d
Gil Neto <gil@opplane.com>
2021-05-18 18:27:09 +0100

    (frontend) added actions' placeholders.

--- frontend/src/app/features/ProjectsList/ProjectsListComponent.jsx
+++ frontend/src/app/features/ProjectsList/ProjectsListComponent.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import styled from "styled-components";
 
 import List from "@material-ui/core/List";
+import Button from "@material-ui/core/Button";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
 
@@ -12,11 +13,18 @@ const Component = ({ projects }) => {
       <Title variant="h4">Projects List</Title>
       <Listing>
         {projects.map((p) => (
-          <ListItem key={p.id} onMouseEnter={() => setHovering(p.id)} onMouseLeave={() => setHovering(undefined)}>
+          <Item key={p.id} onMouseEnter={() => setHovering(p.id)} onMouseLeave={() => setHovering(undefined)}>
             {p.title}
-            {hovering === p.id && <div>aaa</div>}</ListItem>
+            {hovering === p.id && (
+              <Actions>
+                <Button>Edit</Button>
+                <Button>Delete</Button>
+              </Actions>
+            )}
+          </Item>
         ))}
       </Listing>
+      <Button>Add</Button>
     </>
   );
 };
@@ -29,6 +37,17 @@ const Listing = styled(List)`
   overflow-x: hidden;
   overflow-y: auto;
   max-height: 768px;
+`
+
+const Item = styled(ListItem)`
+  min-height: 55px;
+  &:hover {
+    background-color: rgb(0,0,0,0.05);
+  }
+`
+
+const Actions = styled.div`
+  margin-left: auto;
 `
 
 export default Component;
