4a7b7ac969f911b447ffed0d0a05d979103a8605
Gil Neto <gil@opplane.com>
2021-05-19 10:53:31 +0100

    added edit modal with necessary fields.

---
+++ frontend/src/app/features/ProjectsList/EditModalComponent.jsx
@@ -0,0 +1,63 @@
+import {Backdrop} from "@material-ui/core";
+import Fade from "@material-ui/core/Fade";
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { MuiPickersUtilsProvider, DateTimePicker } from "@material-ui/pickers";
+import MomentUtils from '@date-io/moment';
+const moment = require('moment');
+
+const EditModalComponent = ({ project, open, onClose, onSave }) => {
+  console.log('start', project);
+  const [title, setTitle] = useState(undefined);
+  const [startDate, setStartDate] = useState(undefined);
+  const [endDate, setEndDate] = useState(undefined);
+
+  useEffect(() => {
+    setTitle(project.title);
+    setStartDate(moment(project.start_date));
+    setEndDate(moment(project.end_date));
+  }, []);
+
+  const save = (e) => {
+    e.preventDefault();
+    const data = { title, start_date: startDate.format(), end_date: endDate.format() };
+    console.log(data);
+    onSave(data);
+  }
+
+  return (
+    <EditModal open={open} onClose={onClose} closeAfterTransition BackdropComponent={Backdrop} BackdropProps={{ timeout: 500 }}>
+      <Fade in={open}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <ModalContent>
+            <TextField id={'title'} error={!title} required label={'Title'} value={title} onChange={(e) => setTitle(e.target.value)} />
+            <DateTimePicker id={'start_date'} error={!startDate} required label={'Start Date'} value={startDate} onChange={setStartDate} ampm={false} />
+            <DateTimePicker id={'end_date'} error={!endDate} required label={'End Date'} value={endDate} onChange={setEndDate} ampm={false} />
+            <Button disabled={!title} onClick={save}>Save</Button>
+          </ModalContent>
+        </MuiPickersUtilsProvider>
+      </Fade>
+    </EditModal>
+  )
+}
+
+const EditModal = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-flow: column;
+  background-color: rgb(255,255,255,0.8);
+  padding: 5px;
+  & > *:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`
+
+export default EditModalComponent
--- frontend/src/app/features/ProjectsList/ProjectsListComponent.jsx
+++ frontend/src/app/features/ProjectsList/ProjectsListComponent.jsx
@@ -6,11 +6,27 @@ import Button from "@material-ui/core/Button";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
+import EditModalComponent from "./EditModalComponent";
 
 const Component = ({ projects, deleteProject, addProject }) => {
   const [hovering, setHovering] = useState(undefined);
+  const [open, setOpen] = useState(false);
+  const [current, setCurrent] = useState(undefined);
   const dateOptions = {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+
+
   const format = (d) =>  new Date(d).toLocaleDateString('en-UK', dateOptions)
+
+  const handleOpen = (p) => {
+    setOpen(true);
+    setCurrent(p);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+    setCurrent(undefined);
+  }
+
   return (
     <>
       <Title variant="h4">Projects List</Title>
@@ -25,7 +41,7 @@ const Component = ({ projects, deleteProject, addProject }) => {
             </div>
             {hovering === p.id && (
               <Actions>
-                <Button>Edit</Button>
+                <Button onClick={() => handleOpen(p)}>Edit</Button>
                 <Button onClick={() => deleteProject(p.id)}>Delete</Button>
               </Actions>
             )}
@@ -33,6 +49,7 @@ const Component = ({ projects, deleteProject, addProject }) => {
         ))}
       </Listing>
       <ButtonAdd onClick={() => addProject({author: 'aaa', title: 'aaa', start_date: new Date().toISOString(), end_date: new Date().toISOString() })}>Add</ButtonAdd>
+      {current && <EditModalComponent project={current} open={open} onClose={handleClose} onSave={console.log}/>}
     </>
   );
 };
@@ -56,6 +73,7 @@ const Item = styled(ListItem)`
   &:hover {
     background-color: rgb(0,0,0,0.05);
   }
+  margin-bottom: 15px;
 `
 
 const Actions = styled.div`
